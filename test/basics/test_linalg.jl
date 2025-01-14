98d5c6a70937d210cfcfec01b8ff04a4e21fdfa5
Lukas Devos <ldevos98@gmail.com>
2025-01-14 10:53:39 -0500

    Make random tests reproducible

--- test/basics/test_linalg.jl
+++ test/basics/test_linalg.jl
@@ -1,13 +1,16 @@
 using SparseArraysBase: SparseArrayDOK
 using LinearAlgebra: mul!
-using Random
+using Random: Random
+using StableRNGs: StableRNG
+
+const rng = StableRNG(123)
 
 # TODO: add this to main package
-function sprand(::Type{T}, sz::Base.Dims; p::Real=0.5) where {T}
+function sprand(rng::Random.AbstractRNG, ::Type{T}, sz::Base.Dims; p::Real=0.5) where {T}
   A = SparseArrayDOK{T}(undef, sz)
   for I in eachindex(A)
-    if rand() < p
-      A[I] = rand(T)
+    if rand(rng) < p
+      A[I] = rand(rng, T)
     end
   end
   return A
@@ -20,15 +23,15 @@ end
   szC = (szA[1], szB[2])
 
   for p in 0.0:0.25:1
-    C = sprand(T, szC; p)
-    A = sprand(T, szA; p)
-    B = sprand(T, szB; p)
+    C = sprand(rng, T, szC; p)
+    A = sprand(rng, T, szA; p)
+    B = sprand(rng, T, szB; p)
 
     check1 = mul!(Array(C), Array(A), Array(B))
     @test mul!(copy(C), A, B) ≈ check1
 
-    α = rand()
-    β = rand()
+    α = rand(rng, T)
+    β = rand(rng, T)
     check2 = mul!(Array(C), Array(A), Array(B), α, β)
     @test mul!(copy(C), A, B, α, β) ≈ check2
   end
