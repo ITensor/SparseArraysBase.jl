f44f3f25ce6167d7301000f5a502b43d47cac59a
Matt Fishman <mtfishman@users.noreply.github.com>
2025-02-02 21:46:33 -0500

    Introduce `OneElementArray` (#26)

--- test/test_diagonal.jl
+++ test/test_diagonal.jl
@@ -11,13 +11,22 @@ using SparseArraysBase:
 
 using Test: @test, @testset
 
+# compat with LTS:
+@static if VERSION ≥ v"1.11"
+  _diagind = diagind
+else
+  function _diagind(x::Diagonal, ::IndexCartesian)
+    return view(CartesianIndices(x), diagind(x))
+  end
+end
+
 elts = (Float32, Float64, Complex{Float32}, Complex{Float64})
 
 @testset "Diagonal{$T}" for T in elts
   L = 4
   D = Diagonal(rand(T, 4))
   @test storedlength(D) == 4
-  @test eachstoredindex(D) == diagind(D, IndexCartesian())
+  @test eachstoredindex(D) == _diagind(D, IndexCartesian())
   @test isstored(D, 2, 2)
   @test getstoredindex(D, 2, 2) == D[2, 2]
   @test !isstored(D, 2, 1)
