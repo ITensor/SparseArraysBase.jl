13f06a3093b9753b93319862d4b0054426082244
Lukas Devos <ldevos98@gmail.com>
2025-02-18 07:42:35 -0500

    replace AbstractArrayOps with manual `@derive`

--- src/abstractsparsearray.jl
+++ src/abstractsparsearray.jl
@@ -10,20 +10,42 @@ function DerivableInterfaces.interface(::Type{<:AbstractSparseArray})
 end
 
 using DerivableInterfaces: @derive
-
-# TODO: These need to be loaded since `AbstractArrayOps`
-# includes overloads of functions from these modules.
-# Ideally that wouldn't be needed and can be circumvented
-# with `GlobalRef`.
 using ArrayLayouts: ArrayLayouts
 using LinearAlgebra: LinearAlgebra
 
-# DerivableInterfaces `Base.getindex`, `Base.setindex!`, etc.
-# TODO: Define `AbstractMatrixOps` and overload for
-# `AnyAbstractSparseMatrix` and `AnyAbstractSparseVector`,
-# which is where matrix multiplication and factorizations
-# should go.
-@derive AnyAbstractSparseArray AbstractArrayOps
+# This type alias is a temporary workaround since `@derive`
+# doesn't parse the `@MIME_str` macro properly at the moment.
+const MIMEtextplain = MIME"text/plain"
+
+@derive (T=AnyAbstractSparseArray,) begin
+  Base.getindex(::T, ::Any...)
+  Base.getindex(::T, ::Int...)
+  Base.setindex!(::T, ::Any, ::Any...)
+  Base.setindex!(::T, ::Any, ::Int...)
+  Base.similar(::T, ::Type, ::Tuple{Vararg{Int}})
+  Base.similar(::T, ::Type, ::Tuple{Base.OneTo,Vararg{Base.OneTo}})
+  Base.copy(::T)
+  Base.copy!(::AbstractArray, ::T)
+  Base.copyto!(::AbstractArray, ::T)
+  Base.map(::Any, ::T...)
+  Base.map!(::Any, ::AbstractArray, ::T...)
+  Base.mapreduce(::Any, ::Any, ::T...; kwargs...)
+  Base.reduce(::Any, ::T...; kwargs...)
+  Base.all(::Function, ::T)
+  Base.all(::T)
+  Base.iszero(::T)
+  Base.real(::T)
+  Base.fill!(::T, ::Any)
+  ArrayLayouts.zero!(::T)
+  Base.zero(::T)
+  Base.permutedims!(::Any, ::T, ::Any)
+  Broadcast.BroadcastStyle(::Type{<:T})
+  Base.copyto!(::T, ::Broadcast.Broadcasted{Broadcast.DefaultArrayStyle{0}})
+  Base.cat(::T...; kwargs...)
+  ArrayLayouts.MemoryLayout(::Type{<:T})
+  LinearAlgebra.mul!(::AbstractMatrix, ::T, ::T, ::Number, ::Number)
+  Base.show(::IO, ::MIMEtextplain, ::T)
+end
 
 function Base.replace_in_print_matrix(
   A::AnyAbstractSparseArray{<:Any,2}, i::Integer, j::Integer, s::AbstractString
