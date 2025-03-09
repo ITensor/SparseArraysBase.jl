5a9f1e68d8c0bc8d49d8eab16bf61fc0ff787f44
mtfishman <mfishman@flatironinstitute.org>
2025-03-09 14:53:40 -0400

    Improve `isstored` logic

--- src/abstractsparsearrayinterface.jl
+++ src/abstractsparsearrayinterface.jl
@@ -35,23 +35,22 @@ end
 
 # Minimal interface for `SparseArrayInterface`.
 # Fallbacks for dense/non-sparse arrays.
-@interface ::AbstractArrayInterface function isstored(
-  a::AbstractArray{<:Any,N}, I::Vararg{Int,N}
-) where {N}
+function isstored(a::AbstractArray{<:Any,N}, I::Vararg{Int,N}) where {N}
   @_propagate_inbounds_meta
   @boundscheck checkbounds(a, I...)
   return true
 end
-@interface interface::AbstractArrayInterface function isstored(a::AbstractArray, I::Int)
+function isstored(a::AbstractArray, I::Int)
   @_propagate_inbounds_meta
-  return @interface interface isstored(a, Tuple(CartesianIndices(a)[I])...)
+  return isstored(a, Tuple(CartesianIndices(a)[I])...)
 end
-@interface interface::AbstractArrayInterface function isstored(a::AbstractArray, I::Int...)
+function isstored(a::AbstractArray, I::Int...)
   @_propagate_inbounds_meta
   @boundscheck checkbounds(a, I...)
   I′ = ntuple(i -> I[i], ndims(a))
-  return @inbounds @interface interface isstored(a, I′...)
+  return isstored(a, I′...)
 end
+
 @interface ::AbstractArrayInterface eachstoredindex(a::AbstractArray) = eachindex(a)
 @interface ::AbstractArrayInterface getstoredindex(a::AbstractArray, I::Int...) =
   getindex(a, I...)
@@ -111,7 +110,6 @@ end
   SparseArraysBase.eachstoredindex(::T...)
   SparseArraysBase.getstoredindex(::T, ::Int...)
   SparseArraysBase.getunstoredindex(::T, ::Int...)
-  SparseArraysBase.isstored(::T, ::Int...)
   SparseArraysBase.setstoredindex!(::T, ::Any, ::Int...)
   SparseArraysBase.setunstoredindex!(::T, ::Any, ::Int...)
   SparseArraysBase.storedlength(::T)
