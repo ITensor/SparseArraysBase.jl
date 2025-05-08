24c3422333691b9266951f3296fd04445661264a
Lukas Devos <ldevos98@gmail.com>
2025-05-08 14:51:24 -0400

    Don't overtest

--- test/test_linalg.jl
+++ test/test_linalg.jl
@@ -29,17 +29,16 @@ const rng = StableRNG(123)
   szA = (2, 0)
   szB = (0, 2)
   szC = (szA[1], szB[2])
-  for density in 0.0:0.25:1
-    C = sparserand(rng, T, szC; density)
-    A = sparserand(rng, T, szA; density)
-    B = sparserand(rng, T, szB; density)
+  density = 0.1
+  C = sparserand(rng, T, szC; density)
+  A = sparserand(rng, T, szA; density)
+  B = sparserand(rng, T, szB; density)
 
-    check1 = mul!(Array(C), Array(A), Array(B))
-    @test mul!(copy(C), A, B) ≈ check1
+  check1 = mul!(Array(C), Array(A), Array(B))
+  @test mul!(copy(C), A, B) ≈ check1
 
-    α = rand(rng, T)
-    β = rand(rng, T)
-    check2 = mul!(Array(C), Array(A), Array(B), α, β)
-    @test mul!(copy(C), A, B, α, β) ≈ check2
-  end
+  α = rand(rng, T)
+  β = rand(rng, T)
+  check2 = mul!(Array(C), Array(A), Array(B), α, β)
+  @test mul!(copy(C), A, B, α, β) ≈ check2
 end
