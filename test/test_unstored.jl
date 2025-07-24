9f2cb92409236287e22c2f7b9b2cfd097e469200
mtfishman <mfishman@flatironinstitute.org>
2025-07-24 13:12:18 -0400

    Add tests

---
+++ test/test_unstored.jl
@@ -0,0 +1,11 @@
+using SparseArraysBase: Unstored
+using FillArrays: Zeros
+using Test: @test, @testset
+
+@testset "Unstored" begin
+  a = Zeros(2, 2)
+  u = Unstored(a)
+  @test parent(u) ≡ a
+  @test size(u) ≡ size(a)
+  @test axes(u) ≡ axes(a)
+end
