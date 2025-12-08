c4f011c73457470c5469058137b35a4d0b3e6f86
mtfishman <mfishman@flatironinstitute.org>
2025-12-08 12:53:43 -0500

    Add tests

--- src/sparsearrays.jl
+++ src/sparsearrays.jl
@@ -5,7 +5,7 @@ function eachstoredindex(m::AbstractSparseMatrixCSC)
     # TODO: This loses the compile time element type, is there a better lazy way?
     return Iterators.map(CartesianIndex, zip(I, J))
 end
-function eachstoredindex(a::Base.ReshapedArray{<:Any, <:Any, <: AbstractSparseMatrixCSC})
+function eachstoredindex(a::Base.ReshapedArray{<:Any, <:Any, <:AbstractSparseMatrixCSC})
     return @interface SparseArrayInterface() eachstoredindex(a)
 end
 
@@ -17,16 +17,16 @@ function SparseArrays.SparseMatrixCSC{Tv, Ti}(m::AnyAbstractSparseMatrix) where 
     return m′
 end
 
-function SparseArrayDOK(a::Base.ReshapedArray{<:Any, <:Any, <: AbstractSparseMatrixCSC})
+function SparseArrayDOK(a::Base.ReshapedArray{<:Any, <:Any, <:AbstractSparseMatrixCSC})
     return SparseArrayDOK{eltype(a), ndims(a)}(a)
 end
 function SparseArrayDOK{T}(
-        a::Base.ReshapedArray{<:Any, <:Any, <: AbstractSparseMatrixCSC}
+        a::Base.ReshapedArray{<:Any, <:Any, <:AbstractSparseMatrixCSC}
     ) where {T}
     return SparseArrayDOK{T, ndims(a)}(a)
 end
 function SparseArrayDOK{T, N}(
-        a::Base.ReshapedArray{<:Any, N, <: AbstractSparseMatrixCSC}
+        a::Base.ReshapedArray{<:Any, N, <:AbstractSparseMatrixCSC}
     ) where {T, N}
     a′ = SparseArrayDOK{T, N}(undef, size(a))
     for I in eachstoredindex(a)
