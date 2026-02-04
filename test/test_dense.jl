69584285640a487449dad0304efabd6e7710f23e
mtfishman <mfishman@flatironinstitute.org>
2026-02-04 14:35:42 -0500

    Fix tests

--- test/test_dense.jl
+++ test/test_dense.jl
@@ -3,6 +3,21 @@ using JLArrays: JLArray
 using SparseArraysBase: dense, sparsezeros
 using Test: @test, @testset
 
+module TestDenseUtils
+    struct MySparseArrayDOK{T, N, S <: AbstractVector{T}} <: AbstractArray{T, N}
+        storedvalues::S
+        storedindices::Dict{CartesianIndex{N}, Int}
+        size::NTuple{N, Int}
+    end
+    Base.size(a::MySparseArrayDOK) = a.size
+    function Base.getindex(a::MySparseArrayDOK{<:Any, N}, I::Vararg{Int, N}) where {N}
+        storageindex = get(a.storedindices, CartesianIndex(I), nothing)
+        isnothing(storageindex) && return zero(eltype(a))
+        return a.storedvalues[storageindex]
+    end
+    Base.parent(a::MySparseArrayDOK) = a.storedvalues
+end
+
 elts = (Float32, ComplexF64)
 arrayts = (Array, JLArray)
 @testset "dense (arraytype=$arrayt, eltype=$elt)" for arrayt in arrayts, elt in elts
@@ -17,20 +32,7 @@ arrayts = (Array, JLArray)
     end
 
     @testset "Custom sparse array" begin
-        struct MySparseArrayDOK{T, N, S <: AbstractVector{T}} <: AbstractArray{T, N}
-            storedvalues::S
-            storedindices::Dict{CartesianIndex{N}, Int}
-            size::NTuple{N, Int}
-        end
-        Base.size(a::MySparseArrayDOK) = a.size
-        function Base.getindex(a::MySparseArrayDOK{<:Any, N}, I::Vararg{Int, N}) where {N}
-            storageindex = get(a.storedindices, CartesianIndex(I), nothing)
-            isnothing(storageindex) && return zero(eltype(a))
-            return a.storedvalues[storageindex]
-        end
-        Base.parent(a::MySparseArrayDOK) = a.storedvalues
-
-        s = MySparseArrayDOK(
+        s = TestDenseUtils.MySparseArrayDOK(
             dev(elt[2, 4]), Dict([CartesianIndex(1, 2) => 1, CartesianIndex(3, 4) => 2]), (3, 4)
         )
         d = dense(s)
